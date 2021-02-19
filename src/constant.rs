2924599f8860a44c7dc86573bdf317fdb4c20d29
Hsin Yi, Chen <ossug.hychen@gmail.com>
2021-02-19 17:29:23 +0800

    Hard code constants.

--- src/constant.rs
+++ src/constant.rs
@@ -3,13 +3,13 @@
 //! with periods or shares, yet big enough to not limit reasonable use cases.
 
 /// maximum length of voting period
-pub const MAX_VOTING_PERIOD_LENGTH: u128 = 10 ^ 18;
+pub const MAX_VOTING_PERIOD_LENGTH: u128 = 1000000000000000000;
 /// maximum length of grace period
-pub const MAX_GRACE_PERIOD_LENGTH: u128 = 10 ^ 18;
+pub const MAX_GRACE_PERIOD_LENGTH: u128 = 1000000000000000000;
 /// maximum dilution bound
-pub const MAX_DILUTION_BOUND: u128 = 10 ^ 18;
+pub const MAX_DILUTION_BOUND: u128 = 1000000000000000000;
 /// maximum number of shares that can be minted
-pub const MAX_NUMBER_OF_SHARES_AND_LOOT: u128 = 10 ^ 18;
+pub const MAX_NUMBER_OF_SHARES_AND_LOOT: u128 = 1000000000000000000;
 /// maximum number of whitelisted tokens
 pub const MAX_TOKEN_WHITELIST_COUNT: u128 = 400;
 /// maximum number of tokens with non-zero balance in guildbank
